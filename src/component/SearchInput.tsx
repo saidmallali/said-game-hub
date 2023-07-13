751960a31ab54827a97247fdf03c5b7018d9d878
saidmallali <said.labriki@gmail.com>
2023-07-13 18:58:00 +0200

    fix the navbare

--- src/component/SearchInput.tsx
+++ src/component/SearchInput.tsx
@@ -2,15 +2,18 @@ import { FormEvent, useRef } from "react";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
 
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const handelSubmit = (event: FormEvent) => {
     event.preventDefault();
     if (ref.current) setSearchText(ref.current.value);
+    navigate("/");
   };
 
   return (
